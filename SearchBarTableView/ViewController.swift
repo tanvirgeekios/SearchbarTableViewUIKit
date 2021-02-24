e0277d2ec7e2b4f865e3057f92eb1fb9df4f55c0
tanvirgeekios <tanvir.alam@iosventure.com>
2021-02-24 09:37:18 +0600

    Searchbar Working

--- SearchBarTableView/ViewController.swift
+++ SearchBarTableView/ViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
+    
+    let fruits = ["Mango","Apple","Banana","Pinapple","Coconut","Straberry"]
+    var filteredData = [String]()
+    @IBOutlet weak var searchbar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        searchbar.delegate = self
+        filteredData = fruits
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = filteredData[indexPath.row]
+        return cell
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText == "" {
+            filteredData = fruits
+        }else{
+            filteredData = []
+            for fruit in fruits{
+                if fruit.lowercased().contains(searchText.lowercased()){
+                    filteredData.append(fruit)
+                }
+            }
+        }
+        self.tableView.reloadData()
+    }
 
 }
 
